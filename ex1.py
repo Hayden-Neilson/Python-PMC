d3e4a5a342797ff468aa4d59b319639da593dce0
Hayden-Neilson <haydenneilson5@gmail.com>
2020-06-27 13:07:05 -0600

    list work

--- ex1.py
+++ ex1.py
@@ -7,3 +7,12 @@ mytext = "hippy"
 
 print(mynum, mytext)
 
+str = "8"
+float = 10.2
+x = 8
+
+# make a list with  a float a int a str
+temperatures = [1, 29.7, "34"]
+
+# make a list that has a integer float and a str in list
+rainfall = [1, 29.7, "34",  [23, 34, 12]]
