74686581a5553d06ca2d7969ab8e0c066004ddaa
Hayden-Neilson <haydenneilson5@gmail.com>
2020-06-29 21:18:33 -0600

    max method

--- ex1.py
+++ ex1.py
@@ -11,8 +11,13 @@ str = "8"
 float = 10.2
 x = 8
 
-# make a list with  a float a int a str
-temperatures = [1, 29.7, "34"]
+# # make a list with  a float a int a str
+# temperatures = [1, 29.7, "34"]
 
-# make a list that has a integer float and a str in list
-rainfall = [1, 29.7, "34",  [23, 34, 12]]
+# # make a list that has a integer float and a str in list
+# rainfall = [1, 29.7, "34",  [23, 34, 12]]
+
+# max method
+# student_grades = [9.1, 8.8, 7.5]
+# max_value = max(student_grades)
+# print(max_value)
