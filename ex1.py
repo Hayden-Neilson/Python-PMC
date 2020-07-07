84710204efa0f6b081aa435ad7c19c3f2ca82e86
Hayden-Neilson <haydenneilson5@gmail.com>
2020-07-06 20:14:23 -0600

    printing certain parts

--- ex1.py
+++ ex1.py
@@ -49,3 +49,6 @@ x = 8
 # seconds = [1.2323442655, 1.4534345567, 1.023458894]
 # current = 1.10001399445
 # seconds.append(current)
+
+# serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+# print(serials[2])
