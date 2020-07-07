6db5c9444d3b4183a689a9bcae84b0c312c1abdc
Hayden-Neilson <haydenneilson5@gmail.com>
2020-07-06 20:21:15 -0600

    append method

--- ex1.py
+++ ex1.py
@@ -56,3 +56,7 @@ x = 8
 # serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
 # print(serials[0], serials[2], serials[5])
 # 
+
+# workdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+# weekend = ["Sat", "Sun"]
+# workdays.append(weekend[0])
