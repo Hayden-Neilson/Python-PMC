3a593f6a741911cf16633641f5994a436cecb20e
Hayden-Neilson <haydenneilson5@gmail.com>
2020-08-04 15:58:39 -0600

    looped int and colors

--- ex2.py
+++ ex2.py
@@ -6,8 +6,17 @@
 
 # more prctice print colors if greater than 50!
 
-colors = [11, 34, 98, 43, 45, 54, 54]
+# colors = [11, 34, 98, 43, 45, 54, 54]
 
-for color in colors : 
-    if color  > 50:
-        print(color)
+# for color in colors : 
+#     if color  > 50:
+#         print(color)
+
+
+# loop int and big colors
+
+# colors = [11, 34.1, 98.2, 43, 45.1, 54, 54]
+
+# for color in colors:
+#     if isinstance(color, int) and color > 50:
+#         print(color)
