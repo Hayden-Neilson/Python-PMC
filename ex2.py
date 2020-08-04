18ad89c1e51e367c01312e64fca6db49aff5e5c1
Hayden-Neilson <haydenneilson5@gmail.com>
2020-08-04 16:15:27 -0600

    looping for the int

--- ex2.py
+++ ex2.py
@@ -20,3 +20,11 @@
 # for color in colors:
 #     if isinstance(color, int) and color > 50:
 #         print(color)
+
+# looping colors but just getting the integer
+
+# colors = [11, 34.1, 98.2, 43, 45.1, 54, 54]
+
+# for color in colors: 
+#     if isinstance(color, int):
+#         print(color)
