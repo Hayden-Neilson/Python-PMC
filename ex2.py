dbc93efb5b89cefeeff6bd97ff0264d6bae57ca6
Hayden-Neilson <haydenneilson5@gmail.com>
2020-08-04 15:54:40 -0600

    for loop excercise

--- ex2.py
+++ ex2.py
@@ -3,3 +3,11 @@
 
 # for color in  colors:
 #     print(color)
+
+# more prctice print colors if greater than 50!
+
+colors = [11, 34, 98, 43, 45, 54, 54]
+
+for color in colors : 
+    if color  > 50:
+        print(color)
