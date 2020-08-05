694f0e78314f6cbbee60f822144da56302344fa6
Hayden-Neilson <haydenneilson5@gmail.com>
2020-08-05 17:08:59 -0600

    while loop

--- ex2.py
+++ ex2.py
@@ -43,3 +43,8 @@
 
 # for value in phone_numbers.values():
 #     print(value.replace("+", "00"))
+
+# ex of a while loop
+
+# while datetime.datetime.now() < datetime.datetime(2090, 8, 20, 19, 30, 20):
+    # print("It's not yet 19:30:20 of 2090.8.20")
