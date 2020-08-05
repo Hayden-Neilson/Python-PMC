95ec2d4d8443ad4f83eb4b8e9a7c330e5c6eb91b
Hayden-Neilson <haydenneilson5@gmail.com>
2020-08-04 21:54:15 -0600

    replacing a value in a loop

--- ex2.py
+++ ex2.py
@@ -36,3 +36,10 @@
 
 # for key, value in phone_numbers.items():
 #     print("%s: %s" % (key, value))
+
+# replacing a variable with a new string
+
+# phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+
+# for value in phone_numbers.values():
+#     print(value.replace("+", "00"))
