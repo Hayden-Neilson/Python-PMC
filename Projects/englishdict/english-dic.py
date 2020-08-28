bf05e1887755eb6eb31e0fe7d6046bae21571cc8
Hayden-Neilson <haydenneilson5@gmail.com>
2020-08-28 05:23:14 -0600

    added another if and else statment

--- Projects/englishdict/english-dic.py
+++ Projects/englishdict/english-dic.py
@@ -9,7 +9,14 @@ def translate(word):
     if word in data:
         return data(word)
     elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean this instead?" % get_close_matches(word, data.keys()[0])
+        yn = "Did you mean this instead? Enter Y if yes, or N if no" % get_close_matches(
+            word, data.keys()[0])
+        if yn == "Y":
+            return data(get_close_matches(word, data.keys()[0]))
+        elif yn == "N":
+            return "the word is not in the dictionary. Please try another word!"
+        else:
+            return "we didnt understand yout entry "
 
     else:
         return "the word is not in the dictionary. Please try another word!"
