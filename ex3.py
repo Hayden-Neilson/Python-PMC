e5ff8a4f65057eaadcd5c8b9f77a11e18860b014
Hayden-Neilson <haydenneilson5@gmail.com>
2020-10-11 15:12:07 -0600

    error practice

--- ex3.py
+++ ex3.py
@@ -1,1 +1,3 @@
 # learned about what is pandas and the library about what it does
+# how to create a dataframe and addalist with columnsalong with howitworks
+# they work with series and it is like  a data type
