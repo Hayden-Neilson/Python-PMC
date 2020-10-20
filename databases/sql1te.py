3b2d578f199cd9e83a5724eddeaa604dbe407203
Hayden-Neilson <haydenneilson5@gmail.com>
2020-10-19 21:37:46 -0600

    database

---
+++ databases/sql1te.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect("lite.db")
+cur = conn.cursor()
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS store(item TEXT, quaninty INTEGER, price REAL)")
+conn.execute("INSERT INTO store VALUES ('Wine Glass', 8,10.5)")
+conn.commit()
+conn.close()
