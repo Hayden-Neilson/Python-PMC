41e27185288e0c9f899eff8017057adabc975635
Hayden-Neilson <haydenneilson5@gmail.com>
2020-10-20 15:42:07 -0600

    added the functions for insert and create table

--- databases/sql1te.py
+++ databases/sql1te.py
@@ -1,9 +1,18 @@
 import sqlite3
 
-conn = sqlite3.connect("lite.db")
-cur = conn.cursor()
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS store(item TEXT, quaninty INTEGER, price REAL)")
-conn.execute("INSERT INTO store VALUES ('Wine Glass', 8,10.5)")
-conn.commit()
-conn.close()
+
+def create_table():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS store(item TEXT, quaninty INTEGER, price REAL)")
+    conn.commit()
+    conn.close()
+
+
+def insert():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    conn.execute("INSERT INTO store VALUES ('Wine Glass', 8,10.5)")
+    conn.commit()
+    conn.close()
