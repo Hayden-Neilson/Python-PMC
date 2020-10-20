e6178bf16d0fb8a0ea00a3ff9ae01ac24fd32537
Hayden-Neilson <haydenneilson5@gmail.com>
2020-10-20 16:00:41 -0600

    added a way to see

--- databases/sql1te.py
+++ databases/sql1te.py
@@ -5,14 +5,25 @@ def create_table():
     conn = sqlite3.connect("lite.db")
     cur = conn.cursor()
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS store(item TEXT, quaninty INTEGER, price REAL)")
+        "CREATE TABLE IF NOT EXISTS store(item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
 
-def insert():
+def insert(item, quantity, price):
     conn = sqlite3.connect("lite.db")
     cur = conn.cursor()
     conn.execute("INSERT INTO store VALUES ('Wine Glass', 8,10.5)")
     conn.commit()
     conn.close()
+
+
+def view():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    conn.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+    print(view())
