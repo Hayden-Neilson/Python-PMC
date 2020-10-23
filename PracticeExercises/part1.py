3ed28263e84bebd1fa7b64c80f0ed875cbbba569
Hayden-Neilson <haydenneilson5@gmail.com>
2020-10-23 08:15:24 -0600

    started doing some practice

---
+++ PracticeExercises/part1.py
@@ -0,0 +1,22 @@
+#  Write a Python program to get the Python version you are using
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
+
+#  Write a Python program to print the following string in a specific format (see the output):
+
+print("Twinkle, twinkle, little star,\n How I wonder what you are! \n Up above the world so high, Like a diamond in the sky.\n Twinkle, twinkle, little star,\n How I wonder what you are")
+
+# Write a Python program to display the current date and time.
+import datetime
+now = datetime.datetime.now()
+print("current time and date:")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+# Write a Python program which accepts the radius of a circle from the user and compute the area.
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
